076a73185b982748ea69113ba2d9914b467f58e2
l1007 <95461996+l1007@users.noreply.github.com>
2022-03-05 16:48:41 +0800

    Create ds2.php

---
+++ ds2.php
@@ -0,0 +1,42 @@
+{ 常看-MY 
+#常看头# 
+CCTV1HD,http://117.148.179.155/PLTV/88888888/224/3221231468/index.m3u8# 
+CCTV1HD,http://117.148.179.155/PLTV/88888888/224/3221231471/index.m3u8# 
+CCTV-1综合,http://183.207.248.71/cntv/live1/HD-8000k-1080P-cctv1/HD-8000k-1080P-cctv1# 
+CCTV2HD,http://117.148.179.156/PLTV/88888888/224/3221232362/index.m3u8# 
+CCTV2HD,http://117.148.179.147/PLTV/88888888/224/3221231678/index.m3u8# 
+CCTV3HD,http://117.148.179.183/PLTV/88888888/224/3221231682/index.m3u8# 
+CCTV3,http://117.148.179.140/PLTV/88888888/224/3221231528/index.m3u8# 
+CCTV4HD,http://117.148.179.182/PLTV/88888888/224/3221231726/index.m3u8# 
+CCTV4-欧洲HD,http://117.148.179.162/PLTV/88888888/224/3221231615/index.m3u8# 
+CCTV4-美洲HD,http://117.148.179.160/PLTV/88888888/224/3221231654/index.m3u8# 
+CCTV5HD,http://117.148.179.165/PLTV/88888888/224/3221231702/index.m3u8# 
+CCTV5HD,http://117.148.179.142/PLTV/88888888/224/3221232440/index.m3u8# 
+CCTV5,http://117.148.179.139/PLTV/88888888/224/3221231541/index.m3u8# 
+CCTV5+HD,http://117.148.179.136/PLTV/88888888/224/3221231459/index.m3u8# 
+CCTV5+HD,http://117.148.179.139/PLTV/88888888/224/3221231535/index.m3u8# 
+CCTV6HD,http://117.148.179.153/PLTV/88888888/224/3221231724/index.m3u8# 
+CCTV7HD,http://117.148.179.160/PLTV/88888888/224/3221231633/index.m3u8# 
+CCTV8HD,http://117.148.179.160/PLTV/88888888/224/3221231694/index.m3u8# 
+CCTV9HD,http://117.148.179.162/PLTV/88888888/224/3221231697/index.m3u8# 
+CCTV10HD,http://117.148.179.164/PLTV/88888888/224/3221232492/index.m3u8# 
+CCTV10HD,http://117.148.179.175/PLTV/88888888/224/3221231666/index.m3u8# 
+CCTV11HD,http://117.148.179.164/PLTV/88888888/224/3221231711/index.m3u8# 
+CCTV12HD,http://117.148.179.150/PLTV/88888888/224/3221231660/index.m3u8# 
+CCTV13,http://117.148.179.139/PLTV/88888888/224/3221231636/index.m3u8# 
+CCTV14HD,http://117.148.179.182/PLTV/88888888/224/3221231648/index.m3u8# 
+CCTV15HD,http://117.148.179.169/PLTV/88888888/224/3221231693/index.m3u8# 
+CCTV17HD,http://117.148.179.167/PLTV/88888888/224/3221231772/index.m3u8# 
+CCTV-5体育,http://183.207.248.71/cntv/live1/HD-8000k-1080P-cctv5/HD-8000k-1080P-cctv5# 
+CCTV-6电影,http://183.207.248.71/cntv/live1/cctv-6/cctv-6 
+#常看尾# 
+#MY头# 
+TV1,https://rtmlive01tv.secureswiftcontent.com/rtmchannel/01-manifest.mpd# 
+TV2,https://rtmlive05tv.secureswiftcontent.com/rtmchannel/05-manifest.mpd# 
+TV3,https://raw.githubusercontent.com/samleong123/tonton_dailymotion_php/main/m3u8/TV3.m3u8# 
+8TV,https://raw.githubusercontent.com/samleong123/tonton_dailymotion_php/main/m3u8/8TV.m3u8# 
+TV9,https://raw.githubusercontent.com/samleong123/tonton_dailymotion_php/main/m3u8/TV9.m3u8# 
+NTV7,https://raw.githubusercontent.com/samleong123/tonton_dailymotion_php/main/m3u8/DidikTVKPM.m3u8# 
+Berita RTM,https://rtmlive03tv.secureswiftcontent.com/rtmchannel/03-manifest.mpd# 
+Sukan RTM,https://rtmlive06tv.secureswiftcontent.com/rtmchannel/06-manifest.mpd 
+#MY尾# }
